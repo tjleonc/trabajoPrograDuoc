009946897a3cafe36b418f055dd327eceaa7cbbd
tjleonc <137189773+tjleonc@users.noreply.github.com>
2024-05-23 18:17:13 -0400

    v1.0

---
+++ main.js
@@ -0,0 +1,31 @@
+var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+$(document).ready(function() {
+    $("#bEnviar").click(function() {
+        var nombre = $("#name").val();
+        var email = $("#email").val();
+        var mensaje = $("#message").val();
+
+        if(nombre == ""){
+            $("#mensaje1").fadeIn();
+            return false;
+        }else{
+            $("#mensaje1").fadeOut();
+        }
+
+        if(email == "" || !expr.test(email)){
+            $("#mensaje2").fadeIn();
+            return false;
+        }else{
+            $("#mensaje2").fadeOut();
+        }
+
+        if(mensaje == ""){
+            $("#mensaje3").fadeIn();
+            return false;
+        }else{
+            $("#mensaje3").fadeOut();
+        }
+    });
+
+});
